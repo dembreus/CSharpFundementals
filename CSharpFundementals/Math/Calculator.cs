7570d7c0e9a3442344a941b92416a987b36dfa8d
Dillon <dillon.embreus@gmail.com>
2018-11-30 13:59:58 -0700

    started C# course

---
+++ CSharpFundementals/Math/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpFundementals.Math
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ CSharpFundementals/Program.cs
@@ -0,0 +1,19 @@
+﻿using CSharpFundementals.Math;
+using System;
+
+namespace CSharpFundementals
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Add(4,5));
+
+            var person = new Person();
+            person.FirstName = "Rick";
+            person.LastName = "Grimes";
+            person.Introduce();
+        }
+    }
+}
